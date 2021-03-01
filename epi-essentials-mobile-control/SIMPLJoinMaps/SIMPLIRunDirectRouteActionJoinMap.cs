8d92bbfd45858a336967809b12ffc60076c401d5
Andrew Welker <andrew.welker@gmail.com>
2021-03-01 11:52:40 -0700

    Add RunDirectRouteAction Join Map

---
+++ epi-essentials-mobile-control/SIMPLJoinMaps/SIMPLIRunDirectRouteActionJoinMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PepperDash.Essentials.Core;
+
+namespace PepperDash.Essentials.AppServer
+{
+    public class SimpliRunDirectRouteActionJoinMap:JoinMapBaseAdvanced
+    {
+        public SimpliRunDirectRouteActionJoinMap(uint joinStart) : base(joinStart)
+        {
+        }
+
+        public SimpliRunDirectRouteActionJoinMap(uint joinStart, Type type) : base(joinStart, type)
+        {
+        }
+    }
+}
