e0a7be1491e9c6b87421dd90aeed8d4d4b1754c3
Neil Dorin <ndorin@pepperdash.com>
2021-03-30 15:02:21 -0600

    Removes the event handler subscription that was disposing of the queues on program stop

--- epi-essentials-mobile-control/ReceiveQueue.cs
+++ epi-essentials-mobile-control/ReceiveQueue.cs
@@ -33,13 +33,13 @@ namespace PepperDash.Essentials
                 Priority = Thread.eThreadPriority.HighPriority
             };
 
-            CrestronEnvironment.ProgramStatusEventHandler += programEvent =>
-            {
-                if (programEvent != eProgramStatusEventType.Stopping)
-                    return;
+            //CrestronEnvironment.ProgramStatusEventHandler += programEvent =>
+            //{
+            //    if (programEvent != eProgramStatusEventType.Stopping)
+            //        return;
 
-                Dispose();
-            };
+            //    //Dispose();
+            //};
         }
 
         private object ProcessMessage(object obj)
