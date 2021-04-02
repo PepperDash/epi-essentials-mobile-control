6f943838855161faded028d875a6651fa30ca1b7
Andrew Welker <andrew.welker@gmail.com>
2021-04-02 16:01:15 -0600

    add TransmitMessage

---
+++ epi-essentials-mobile-control/TransmitMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using Newtonsoft.Json.Linq;
+using PepperDash.Essentials.Core.Queues;
+using WebSocketSharp;
+
+namespace PepperDash.Essentials
+{
+    public class TransmitMessage:IQueueMessage
+    {
+        private readonly WebSocket _ws;
+        private readonly JObject msgToSend;
+
+        public TransmitMessage(JObject msg, WebSocket ws)
+        {
+            _ws = ws;
+            msgToSend = msg;
+        }
+        #region Implementation of IQueueMessage
+
+        public void Dispatch()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
