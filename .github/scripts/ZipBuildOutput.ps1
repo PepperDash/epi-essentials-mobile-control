6ecd34551d6aad3064a29765d1c745eeb51df70a
Andrew Welker <andrew.welker@gmail.com>
2021-07-16 09:18:49 -0600

    ci: fix path for creating zip

--- .github/scripts/ZipBuildOutput.ps1
+++ .github/scripts/ZipBuildOutput.ps1
@@ -26,7 +26,7 @@ Get-ChildItem -recurse -Path "$($Env:GITHUB_WORKSPACE)" -include "*.clz", "*.cpz
 } | Copy-Item -Destination ($destination) -Force
 Write-Host "Getting matching files..."
 # Get any files from the output folder that match the following extensions
-Get-ChildItem -Path '$($destination)/*' | Where-Object {($_.Extension -eq ".clz") -or ($_.Extension -eq ".cpz" -or ($_.Extension -eq ".cplz"))} | ForEach-Object { 
+Get-ChildItem -Path $destination | Where-Object {($_.Extension -eq ".clz") -or ($_.Extension -eq ".cpz" -or ($_.Extension -eq ".cplz"))} | ForEach-Object { 
   # Replace the extensions with dll and xml and create an array 
   $filenames = @($($_ -replace "cpz|clz|cplz", "dll"), $($_ -replace "cpz|clz|cplz", "xml"))
   Write-Host "Filenames:"
