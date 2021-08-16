060e241d9acf2ed63e1509eb3b466df6ad923c3c
Andrew Welker <awelker@pepperdash.com>
2021-08-16 12:00:38 -0600

    chore: update local build number for assemblyinfo.cs

--- 3-series/Properties/AssemblyInfo.cs
+++ 3-series/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using Crestron.SimplSharp.Reflection;
 [assembly: System.Reflection.AssemblyProduct("EssentialsPluginTemplateEpi")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright ©  2019")]
 [assembly: System.Reflection.AssemblyVersion("3.0.0.*")]
-[assembly: System.Reflection.AssemblyInformationalVersion("3.0.0-alpha-2")]
-[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("3.0.0-alpha-2")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.0.0-local-3")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("3.0.0-local-3")]
